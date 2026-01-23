cab516fd5c9d27fc52c0697f0fba9db8c7fcedee
abhi5hek001 <sahayabhishek.edu@gmail.com>
2026-01-23 14:46:14 +0530

    fix: sse error

--- backend/controllers/stockController.js
+++ backend/controllers/stockController.js
@@ -68,11 +68,10 @@ const stockController = {
      * SSE endpoint for real-time stock updates
      */
     async streamStock(req, res) {
-        // Set headers for SSE
         res.setHeader('Content-Type', 'text/event-stream');
         res.setHeader('Cache-Control', 'no-cache');
         res.setHeader('Connection', 'keep-alive');
-        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('X-Accel-Buffering', 'no'); // Disable Nginx buffering
 
         // Send initial stock data
         try {
